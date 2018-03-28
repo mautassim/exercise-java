65f8051c959f8d3b769263599c537ea24a035f20
mautassim <35828480+mautassim@users.noreply.github.com>
2018-03-29 00:51:51 +0200

    Update find the common elements between tow array.java

--- find the common elements between tow array.java
+++ find the common elements between tow array.java
@@ -24,7 +24,7 @@ for(int i=0 ; i<array1.length ; i++)
 for(int j=0 ; j<array2.length ; j++)
 
 if(array1[i] == array2[j]);
-System.out.print(array1[i] + " - ");
+System.out.print(array1[i] + " and ");
 
 }
 
