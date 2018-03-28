65c51582e2fb96416e5e44bc81c84dd0574541b0
mautassim <35828480+mautassim@users.noreply.github.com>
2018-03-29 01:09:23 +0200

    Update pairs of element in an array.java

--- pairs of element in an array.java
+++ pairs of element in an array.java
@@ -4,7 +4,7 @@ public static void main(String [] args){
 int array [] ={1,2,3,4,5,6};
 int number=7;
 
-System.out.println("paris of elements and their sun");
+System.out.println("paris of elements and their sum");
 
 for(int i=0 ; i<array.length ; i++){
 for(int j = i+1<array.length ; j++)
