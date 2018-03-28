17eba8b2920e74a75f9bf5abce385b769f34a6e8
mautassim <35828480+mautassim@users.noreply.github.com>
2018-03-29 00:32:00 +0200

    Create insert element in to array.java

---
+++ insert element in to array.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class insert{
+piblic static void main(String [] args){
+
+Scanner input = new Scanner(System.in);
+
+int array [] = {1,2,3,4,5,6,7,8,9};
+int intex_position;
+int newelement;
+
+System.out.println("insert position");
+intex_position = input.nextInt();
+
+System.out.println("insert element");
+newelement = input.nextInt();
+
+for(int i =0 ; i<arrat.length ; i++){
+
+System.out.print(array[i] + " ");
+System.out.println();
+
+------------------------------------------------------------------------------------------------
+
+for(int i = array.length -1 ; i> intexposition ; i--){
+
+array[i] = array[i - 1];
+
+}
+
+array[intexposition] = newelement;
+
+for(int i = 0 ; i<array.length ; i++)
+System.out.print(array[i] + " ");
+System.out.println();
+
+}
+
+}
+
+}
