743dbdc465a4f77fcab19b21771540381e99c7ab
mautassim <35828480+mautassim@users.noreply.github.com>
2018-03-28 23:40:52 +0200

    Create unique elements in array.java

---
+++ unique elements in array.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+puplic class unique_elements{
+public static void main(String [] args){
+
+Scanner input = new Scanner(System.in);
+
+int array [] = {1,2,1,3,2,4,3,4};
+
+for(int i=0 ; i<array.length ; i++){
+
+boolean found = false;
+
+for(int j = i-1 ; j>=0 ; j--)
+
+if(array[j] == array[i])
+found == true;
+
+if(! found)  //or used eles
+
+System.out.println(array[j]);//or array[i]
+
+}
+
+}
+
+}
