9e7a9fbc6e593efdfdabdd2957ded175904810dd
mautassim <35828480+mautassim@users.noreply.github.com>
2018-03-29 01:08:50 +0200

    Create pairs of element in an array.java

---
+++ pairs of element in an array.java
@@ -0,0 +1,19 @@
+public class pairs{
+public static void main(String [] args){
+
+int array [] ={1,2,3,4,5,6};
+int number=7;
+
+System.out.println("paris of elements and their sun");
+
+for(int i=0 ; i<array.length ; i++){
+for(int j = i+1<array.length ; j++)
+if(array[i] + array[j] == number)
+
+System.out.println(array[i] + "+" + array[j] + "=" + number);
+
+}
+
+}
+
+}
