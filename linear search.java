7f904cccbe349ede8b7937ff7f02a0444109ac23
mautassim <35828480+mautassim@users.noreply.github.com>
2018-03-29 13:11:23 +0200

    Create linear search.java

---
+++ linear search.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class linear{
+public static void main(String [] args){
+
+Scanner input = new Scanner(System.in);
+
+int array [] ={1,2,3,4,5,6,7,8,9};
+
+System.out.println("insert the key to search");
+int value = input.nextInt();
+
+for(int i=0 ; i<array.length ; i++){
+
+if(array[i] == value){
+
+System.out.println("found");
+
+bteak;
+
+}
+
+}
+
+}
+
+}
+
+}
