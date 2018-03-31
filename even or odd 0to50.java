35d31f05bab374cc8a7bc0647631ad1aebb83d76
mautassim <35828480+mautassim@users.noreply.github.com>
2018-03-31 20:48:07 +0200

    Create even or odd 0to50.java

---
+++ even or odd 0to50.java
@@ -0,0 +1,22 @@
+package javaapplication36;
+
+public class JavaApplication36 {
+
+    public static void main(String[] args) { 
+        
+      for(int i=0 ; i<=50 ; i++){
+      
+      if(i%2 == 0)
+      
+      System.out.println("is even number = " + i + " ");
+      
+      else
+      
+      System.out.println("is odd number = " + i + " ");
+      
+      
+      }      
+      
+    }
+    
+}
